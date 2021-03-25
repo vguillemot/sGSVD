f31c42da8a90b6a6b4eaf953d10f8e986466bdbf
Ju-Chi Yu <23201134+juchiyu@users.noreply.github.com>
2021-03-25 16:04:05 +0800

    Add unique.column function

---
+++ R/unique.column.R
@@ -0,0 +1,14 @@
+#' Exclude duplicated columns with a tolerance
+#'
+#' @param x a marix with columns to screen 
+#' @param n.round to allow tolerance of the difference between values, specify the rounding digit 
+#' @return The unique columns of \eqn{x}.
+#' @examples 
+#' x <- matrix(c(1,2,3, 1.001, 2.002, 3.003), nrow = 3, ncol = 2, byrow = FALSE)
+#' unique.column(x, round = 2
+#' @export
+unique.column <- function(x, n.round) {
+  suppressMessages(require("data.table"))
+  xt.round <- t(round(x, n.round))
+  return(as.matrix(x[,!duplicated(xt.round)]))
+}
