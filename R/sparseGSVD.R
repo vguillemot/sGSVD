738c931fddc9c49ed63648d2ef26fbb65aa08231
Ju-Chi Yu <23201134+juchiyu@users.noreply.github.com>
2021-03-30 13:03:54 +0800

    Add left and right masses for two-table GSVD

--- R/sparseGSVD.R
+++ R/sparseGSVD.R
@@ -40,7 +40,7 @@
 #' @export
 
 
-sparseGSVD <- function(X, Y = NULL, LW, RW, k = 0, tol = .Machine$double.eps,
+sparseGSVD <- function(X, Y = NULL, LW, RW, LM, RM, k = 0, tol = .Machine$double.eps,
                        init = "svd", initLeft = NULL, initRight = NULL, seed = NULL,
                        rdsLeft = rep(1, k), rdsRight = rep(1, k),
                        grpLeft = NULL, grpRight = NULL,
@@ -156,16 +156,96 @@ sparseGSVD <- function(X, Y = NULL, LW, RW, k = 0, tol = .Machine$double.eps,
     }
   }
 
+  # a few things about LM for stopping conditions
+  LM_is_missing <- missing(LM)
+
+  if ( !LM_is_missing ){
+
+    LM_is_vector <- is.vector(LM)
+
+    if ( !LM_is_vector ){
+      if ( Y_is_missing ){
+        if ( nrow(LM) != ncol(LM) | nrow(LM) != X_dimensions[1] ){
+          stop("gsvd: nrow(LM) does not equal ncol(LM) or nrow(X)")
+        }
+      }else{
+        if ( nrow(LM) != ncol(LM) | nrow(LM) != X_dimensions[2] ){
+          stop("gsvd: nrow(LM) does not equal ncol(LM) or ncol(X)")
+        }
+      }
+      # if you gave me all zeros, I'm stopping.
+      if(is_empty_matrix(LM)){
+        stop("gsvd: LM is empty (i.e., all 0s")
+      }
+    }
+
+    if(LM_is_vector){
+      if ( Y_is_missing ){
+        if(length(LM)!=X_dimensions[1]){
+          stop("gsvd: length(LM) does not equal nrow(X)")
+        }
+      }else{
+        if(length(LM)!=X_dimensions[2]){
+          stop("gsvd: length(LM) does not equal ncol(X)")
+        }
+      }
+
+
+      # if you gave me all zeros, I'm stopping.
+      # if(all(abs(LM)<=tol)){
+      if(!are_all_values_positive(LM)){
+        stop("gsvd: LM is not strictly positive values")
+      }
+    }
+  }
+
+  # a few things about RM for stopping conditions
+  RM_is_missing <- missing(RM)
+  if ( !RM_is_missing ){
+
+    RM_is_vector <- is.vector(RM)
+
+    if ( !RM_is_vector ){
+      if ( Y_is_missing ){
+        if( nrow(RM) != ncol(RM) | nrow(RM) != X_dimensions[2] ){
+          stop("gsvd: nrow(RM) does not equal ncol(RM) or ncol(X)")
+        }
+      }else{
+        if( nrow(RM) != ncol(RM) | nrow(RM) != Y_dimensions[2] ){
+          stop("gsvd: nrow(RM) does not equal ncol(RM) or ncol(Y)")
+        }
+      }
+      # if you gave me all zeros, I'm stopping.
+      if(is_empty_matrix(RM)){
+        stop("gsvd: RM is empty (i.e., all 0s")
+      }
+    }
+
+    if(RM_is_vector){
+      if ( Y_is_missing ){
+        if(length(RM)!=X_dimensions[2]){
+          stop("gsvd: length(RM) does not equal ncol(X)")
+        }
+      }else{
+        if(length(RM)!=Y_dimensions[2]){
+          stop("gsvd: length(RM) does not equal ncol(Y)")
+        }
+      }
+
+      # if you gave me all zeros, I'm stopping.
+      # if(all(abs(RM)<=tol)){
+      if(!are_all_values_positive(RM)){
+        stop("gsvd: RM is not strictly positive values")
+      }
+    }
+  }
+
+
 
   ## convenience checks *could* be removed* if problematic
   # convenience checks & conversions; these are meant to minimize LW's memory footprint
   if(!LW_is_missing){
     if( !LW_is_vector){
-
-      # if( is_identity_matrix(LW) ){
-      #   LW_is_missing <- T
-      #   LW <- substitute() # neat! this makes it go missing
-      # }
 
       if( is_diagonal_matrix(LW) ){
         LW <- diag(LW)
@@ -179,14 +259,24 @@ sparseGSVD <- function(X, Y = NULL, LW, RW, k = 0, tol = .Machine$double.eps,
     }
   }
 
+  if(!LM_is_missing){
+    if( !LM_is_vector){
+
+      if( is_diagonal_matrix(LM) ){
+        LM <- diag(LM)
+        LM_is_vector <- T  # now it's a vector
+      }
+    }
+
+    if( LM_is_vector & all(LM==1) ){
+      LM_is_missing <- T
+      LM <- substitute() # neat! this makes it go missing
+    }
+  }
+
   # convenience checks & conversions; these are meant to minimize RW's memory footprint
   if(!RW_is_missing){
     if( !RW_is_vector ){
-
-      # if( is_identity_matrix(RW) ){
-      #   RW_is_missing <- T
-      #   RW <- substitute() # neat! this makes it go missing
-      # }
 
       if( !RW_is_vector & is_diagonal_matrix(RW) ) {
         RW <- diag(RW)
@@ -200,20 +290,33 @@ sparseGSVD <- function(X, Y = NULL, LW, RW, k = 0, tol = .Machine$double.eps,
     }
   }
 
+  if(!RM_is_missing){
+    if( !RM_is_vector ){
+
+      if( !RM_is_vector & is_diagonal_matrix(RM) ) {
+        RM <- diag(RM)
+        RM_is_vector <- TRUE  # now it's a vector
+      }
+    }
+
+    if( RM_is_vector & all(RM == 1) ) {
+      RM_is_missing <- TRUE
+      RM <- substitute() # neat! this makes it go missing
+    }
+  }
+
 
   # this manipulates X as needed based on XLW
   if ( !LW_is_missing ) { ## plain SVD
     if ( LW_is_vector ) {
       sqrt_LW <- sqrt(LW)
-      # X <- sweep(X,1,sqrt_LW,"*") ## replace the sweep with * & t()
       if (Y_is_missing) {   ## one-table
         X <- X * sqrt_LW
       } else {              ## two-table
-        X <- sqrt_LW * X
+        X <- t(t(X) * sqrt_LW)
       }
     } else {              ## GSVD
       LW <- as.matrix(LW)
-      # X <- (LW %^% (1/2)) %*% X
       if (Y_is_missing) {   ## one-table
         X <- sqrt_psd_matrix(LW) %*% X
       } else {              ## two-table
@@ -221,6 +324,19 @@ sparseGSVD <- function(X, Y = NULL, LW, RW, k = 0, tol = .Machine$double.eps,
     }
     }
   }
+
+  if ( !Y_is_missing ) { ## two-tables with masses for X
+    if ( !LM_is_missing ) {
+      if (LM_is_vector) {
+        X <- X * sqrt(LM)
+      }
+      else {
+        LM <- as.matrix(LM)
+        X <- sqrt_psd_matrix(LM) %*% X
+      }
+    }
+  }
+
   # this manipulates X (or Y) as needed based on XRW
   if ( !RW_is_missing ) { ## plain SVD
     if( RW_is_vector ) {
@@ -229,14 +345,26 @@ sparseGSVD <- function(X, Y = NULL, LW, RW, k = 0, tol = .Machine$double.eps,
       if (Y_is_missing) {   ## one-table
         X <- t(t(X) * sqrt_RW)
       } else {              ## two-table
-        Y <- Y * sqrt_RW
+        Y <-  t(t(Y) * sqrt_RW)
       }
     } else {              ## GSVD
       RW <- as.matrix(RW)
       if (Y_is_missing) {   ## one-table
         X <- X %*% sqrt_psd_matrix(RW)
       } else {              ## two-table
-        Y <- sqrt_psd_matrix(RW) %*% Y
+        Y <- Y %*% sqrt_psd_matrix(RW)
+      }
+    }
+  }
+
+  if ( !Y_is_missing ) { ## two-tables with masses for Y
+    if ( !RM_is_missing ) {
+      if (RM_is_vector) {
+        Y <- Y * sqrt(RM)
+      }
+      else {
+        RM <- as.matrix(RM)
+        Y <- sqrt_psd_matrix(RM) %*% Y
       }
     }
   }
--- R/unique.column.R
+++ R/unique.column.R
@@ -1,9 +1,9 @@
 #' Exclude duplicated columns with a tolerance
 #'
-#' @param x a marix with columns to screen 
-#' @param n.round to allow tolerance of the difference between values, specify the rounding digit 
+#' @param x a marix with columns to screen
+#' @param n.round to allow tolerance of the difference between values, specify the rounding digit
 #' @return The unique columns of \eqn{x}.
-#' @examples 
+#' @examples
 #' x <- matrix(c(1,2,3, 1.001, 2.002, 3.003), nrow = 3, ncol = 2, byrow = FALSE)
 #' unique.column(x, round = 2
 #' @export
