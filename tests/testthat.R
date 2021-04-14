34df9222dc850cb00e3ba13b2ba6f169a223f57a
Ju-Chi Yu <23201134+juchiyu@users.noreply.github.com>
2021-04-14 18:23:23 +0800

    Fix testthat

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,5 +1,9 @@
 library(testthat)
-# install.packages("vguillemot/sGSVD")
+# detach("package:sGSVD", unload = TRUE)
+# devtools::install_git(
+#   "https://github.com/vguillemot/sGSVD.git",
+#   credentials = git2r::cred_user_pass("juchiyu", getPass::getPass())
+# )
 library(sGSVD)
 
 test_check("sGSVD")
